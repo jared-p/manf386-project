f912c7f63ca61f68fabd9b2a06091925f1d6f1cf
Jrock <60457530+jared-p@users.noreply.github.com>
2021-03-13 13:38:55 -0800

    delete bad files

--- 109482830_Target_1500S_CODE_V12/MATLAB_Simulink/ModelPID_Target_1500S_V3_0_grt_Output/ext_target1500s.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXT_NONBLOCKING_H_
-#define EXT_NONBLOCKING_H_
-
-extern bool ModelPID_step_ext();
-extern void ModelPID_initialize_ext();
-extern void ModelPID_terminate_ext(bool resetBuffer);
-
-#endif                                 /* EXT_NONBLOCKING_H_ */
