da7ef84909636066245f42fe9b3903b9b56636ef
Jrock <60457530+jared-p@users.noreply.github.com>
2021-03-13 18:44:19 -0800

    fixed html so no jquery is needed

--- website_2/js/index.js
+++ website_2/js/index.js
@@ -1,54 +1,62 @@
-window.addEventListener('load', function(){
-    if( $('#toggle_val').val() == 1){
-        $('#toggle_submit').attr('value', 'Automatic');
-        //$('#toggle_form').css('background-color','Green');
-        $('#toggle_submit').css('background-color','Green');
-        $('#text_input').prop('disabled', true);
-        $('#text_submit').prop('disabled', true);
-        $('#increment').prop('disabled', true);
-        $('#decrement').prop('disabled', true);
-    }else{
-        $('#text_input').prop('disabled', false);
-        $('#text_submit').prop('disabled', false);
-        $('#increment').prop('disabled', false);
-        $('#decrement').prop('disabled', false);
-    }
-    $('#toggle_submit').on('click', function(e){
-        if( e.target.nodeName == 'INPUT')
-            e.preventDefault();
-        var toggleNode = $('#toggle_val');
-        var submit = $('#toggle_submit');
-        var form = $(this);
-        var value = toggleNode.val();
-        if (value == 0){
-            value = 1;
-            submit.attr('value', 'Automatic');
-            //form.css('background-color','Green')
-            submit.css('background-color','green');
-            $('#text_input').prop('disabled', true);
-            $('#text_submit').prop('disabled', true);
-            $('#increment').prop('disabled', true);
-            $('#decrement').prop('disabled', true);
-        }else{
-            value = 0;
-            submit.attr('value', 'Manual');
-            //form.css('background-color','Red')
-            submit.css('background-color','Red');
-            $('#text_input').prop('disabled', false);
-            $('#text_submit').prop('disabled', false);
-            $('#increment').prop('disabled', false);
-            $('#decrement').prop('disabled', false);
-        }
-        toggleNode.val(value);
-        //console.log(toggleNode.val());    
-    });  
+window.addEventListener('load', function () {
+    var toggleForm = document.querySelector('#toggle_form');
+    var toggleSubmit = document.querySelector('#toggle_submit');
+    var toggleVal = document.querySelector('#toggle_val');
+    var textForm = document.querySelector('#text_form');
+    var textSubmit = document.querySelector('#text_submit');
+    var textInput = document.querySelector('#text_input');
+    var increment = document.querySelector('#increment');
+    var decrement = document.querySelector('#decrement');
 
-    $('#text_form').on('submit', function(e){
-        if( $('#toggle_val').val() == 1){
+    if (toggleVal.value == 1) {
+        toggleSubmit.value = "Automatic";
+        toggleSubmit.style.backgroundColor = "green";
+        textSubmit.disabled = true;
+        textInput.disabled = true;
+        increment.disabled = true;
+        decrement.disabled = true;
+    } else {
+        textSubmit.disabled = false;
+        textInput.disabled = false;
+        increment.disabled = false;
+        decrement.disabled = false;
+    }
+    toggleSubmit.addEventListener('click', function (e) {
+        var toggleForm = document.querySelector('#toggle_form');
+        var toggleSubmit = document.querySelector('#toggle_submit');
+        var toggleVal = document.querySelector('#toggle_val');
+        var textForm = document.querySelector('#text_form');
+        var textSubmit = document.querySelector('#text_submit');
+        var textInput = document.querySelector('#text_input');
+        var increment = document.querySelector('#increment');
+        var decrement = document.querySelector('#decrement');
+        if (e.target.nodeName == 'INPUT')
+            e.preventDefault();
+        var value = toggleVal.value;
+        if (value == 0) {
+            value = 1;
+            toggleSubmit.value = "Automatic";
+            toggleSubmit.style.backgroundColor = "green";
+            textSubmit.disabled = true;
+            textInput.disabled = true;
+            increment.disabled = true;
+            decrement.disabled = true;
+        } else {
+            value = 0;
+            toggleSubmit.value = "Manual";
+            toggleSubmit.style.backgroundColor = "red";
+            textSubmit.disabled = false;
+            textInput.disabled = false;
+            increment.disabled = false;
+            decrement.disabled = false;
+        }
+        toggleVal.value = value;
+    });
+    textForm.addEventListener('submit', function(e){
+        var toggleVal = document.querySelector('#toggle_val');
+        if( toggleVal.value == 1){
             e.preventDefault();
             alert("You cannot override in automatic mode");
         }
-    })
-
-    
+    });
 });
